5c18cc2056aaca74fe97f467f628093e1c80c3fa
Glitch20057 <ma6962191@gmail.com>
2026-08-13 14:30:34 +0330

    fix: import ProcessorsModule to activate background workers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,30 +2,27 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { BullModule } from '@nestjs/bullmq';
 import { TaskManagerModule } from './modules/task-manager/task-manager.module';
+import { ProcessorsModule } from './modules/task-manager/processors/jobs.processor.module'; // این خط اضافه شد
 import configuration from './config/configuration';
 
 @Module({
   imports: [
-    // Global Configuration
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
     }),
-    
-    // Redis / Job Queue Setup (Securely connected via ENV)
     BullModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         connection: {
           host: configService.get('redis.host'),
           port: configService.get('redis.port'),
-          password: configService.get('redis.password'), // Optional but recommended
+          password: configService.get('redis.password'),
         },
       }),
     }),
-    
-    // Feature Module
     TaskManagerModule,
+    ProcessorsModule // اینجا هم اضافه شد
   ],
 })
 export class AppModule {}
