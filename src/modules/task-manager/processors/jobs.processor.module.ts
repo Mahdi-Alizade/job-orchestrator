11436d71e6438aca107e782ff5203dc3c4939eff
Glitch20057 <ma6962191@gmail.com>
2026-08-13 14:01:29 +0330

    fix: import TaskManagerModule into ProcessorsModule for DI resolution

--- src/modules/task-manager/processors/jobs.processor.module.ts
+++ src/modules/task-manager/processors/jobs.processor.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JobsProcessor } from './jobs.processor';
+import { TaskManagerModule } from '../task-manager.module';
 
 @Module({
+  imports: [TaskManagerModule], // حالا سرویس در دسترس است
   providers: [JobsProcessor],
   exports: [JobsProcessor],
 })
